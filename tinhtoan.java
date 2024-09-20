fc615ffb6b17b52e738d08e413b0af66f7f8795a
Dinh <dinhknd3@gmail.com>
2024-09-20 08:08:58 +0700

    Bài 2

--- tinhtoan.java
+++ tinhtoan.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+
 public class tinhtoan {
-    
+    public static void main(String[] args) {
+        System.out.println("Hãy nhập thông tin cá nhân của bạn: ");
+        System.out.print("Nhập họ và tên: ");
+        String ten = new Scanner(System.in).nextLine();
+        System.out.print("Nhập tuổi: ");
+        int tuoi = new Scanner(System.in).nextInt();
+        System.out.print("Nhập chiều cao: ");
+        float ccao = new Scanner(System.in).nextFloat();
+        System.out.print("Bạn có thích học lập trình không: ");
+        String dan = new Scanner(System.in).nextLine();
+        System.out.println("Thông tin cá nhân của bạn là: ");
+        System.out.println("Họ tên: " +ten);
+        System.out.println("Tuổi: " +tuoi);
+        System.out.println("Chiều cao: " +ccao);
+        System.out.println(dan+ " thích học lập trình !");
+    }
 }
