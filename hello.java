3017b908c4c7e0abe1900a69835511a25ed19f91
Dinh <dinhknd3@gmail.com>
2024-09-20 08:01:45 +0700

    Bài 1

--- hello.java
+++ hello.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 public class hello {
-    public static void main(String[] args) {
-        System.out.println("Vui lòng nhập tên của bạn: ");
+     public static void main(String[] args) {
+        System.out.print("Vui lòng nhập tên của bạn: ");
         String ten = new Scanner(System.in).nextLine();
         System.out.printf("Hello %s", ten);
     }
