bcc536ca8a3ca61473061a45cd435df514cb1c83
Dinh <dinhknd3@gmail.com>
2024-09-20 14:52:51 +0700

    Xóa file

--- Bai1TH.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Bai1TH {
-    public static void main(String[] args) {
-        System.out.println("Vui lòng nhập tên của bạn: ");
-        String ten = new Scanner(System.in).nextLine();
-        System.out.printf("Hello %s", ten);
-    }
-}
