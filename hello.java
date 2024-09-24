c02f720f3118c9e7f119dece3fe1dc058a2c9b20
Dinh <dinhknd3@gmail.com>
2024-09-24 23:11:38 +0700

    Xoa demo tuan 2

--- hello.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class hello {
-     public static void main(String[] args) {
-        System.out.print("Vui lòng nhập tên của bạn: "); //Nhap jj do di
-        String ten = new Scanner(System.in).nextLine();
-        System.out.printf("Helllo %s", ten);
-    }
-}
