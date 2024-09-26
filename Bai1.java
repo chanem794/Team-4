2245f44c6763613761605284d6be3a74a08997b1
ThoKhang <khangheheqt@gmail.com>
2024-09-26 21:48:08 +0700

    Hoan thien bai 1

--- Bai1.java
+++ Bai1.java
@@ -1,5 +1,20 @@
 // 1.	Viết chương trình kiểm tra số âm, dương hoặc bằng 0.
 
+import java.util.Scanner;
+
 public class Bai1 {
-    //hello
+    import java.util.Scanner;
+
+    public class Bai1 {
+        public static void main(String[] args) {
+            System.out.printf("Vui lòng nhập số cần kiểm tra: "); //Nhập số
+            int check = new Scanner(System.in).nextInt();
+            if(check > 0 )// check
+                System.out.println("Đây là số dương ! ");
+            else if (check < 0)
+                System.out.println("Đây là số âm ! ");
+            else
+                System.out.println("Đây là số 0");
+        }
+    }
 }
