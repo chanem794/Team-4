a63c1e5d4038d46ff8622266ae6c5a1d36355ff2
ThoKhang <khangheheqt@gmail.com>
2024-09-26 22:26:10 +0700

    Chinh sua bai 2

--- Bai2.java
+++ Bai2.java
@@ -1,25 +1,18 @@
 //2.	Xếp loại học sinh: Nhập vào điểm trung bình của một học sinh. 
 //Sử dụng câu lệnh điều kiện để xếp loại học sinh (Giỏi, Khá, Trung bình, Yếu).//
-
 import java.util.Scanner;
 
 public class Bai2 {
     public static void main(String[] args) {
-        System.out.print("Nhap vao diem trung binh cua mot hoc sinh");
-        Scanner sc = new Scanner(System.in);
-        Float diem = sc.nextFloat();
-        if (diem > 8.0) {
-            System.out.println("La hs Gioi");
-        } else {
-            if (diem > 6.5) {
-                System.out.println("La hs Kha");
-            } else {
-                if (diem > 4) {
-                    System.out.println("La hs Trung binh");
-                } else {
-                    System.out.println("La hs Yeu");
-                }
-            }
+        String kq;                                                  //Khai báo biến kết quả để kiểm tra học lực
+        System.out.printf("Vui lòng nhập điểm của sinh viên: ");    //Nhập điểm của sinh viên
+        float check = new Scanner(System.in).nextFloat();           //Khởi Tạo biến điểm
+        if(check <= 10)                                             // kiểm tra xem giá trị nhập vào có phải là giá trị đúng hay không
+        {
+            kq = (10 >= check && check >= 8.5 )? "GIỎI" : (check >= 7.0 ? "KHÁ": (check >= 5.5? "TRUNG BÌNH":(check >= 4.0 ? "YẾU": "RỚT MÔN")));
+            System.out.println("Vậy học sinh này đạt điểm " + kq);
         }
+        else
+            System.out.println("Số bạn nhập vào không đúng");
     }
 }
