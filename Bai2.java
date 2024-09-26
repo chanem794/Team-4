0987201d31be5ef3adfbe2a3e84037f5fb68660b
Dinh <dinhknd3@gmail.com>
2024-09-26 22:18:51 +0700

    Sửa Bài 2

--- Bai2.java
+++ Bai2.java
@@ -1,5 +1,25 @@
 //2.	Xếp loại học sinh: Nhập vào điểm trung bình của một học sinh. 
 //Sử dụng câu lệnh điều kiện để xếp loại học sinh (Giỏi, Khá, Trung bình, Yếu).//
+
+import java.util.Scanner;
+
 public class Bai2 {
-    
+    public static void main(String[] args) {
+        System.out.print("Nhap vao diem trung binh cua mot hoc sinh");
+        Scanner sc = new Scanner(System.in);
+        Float diem = sc.nextFloat();
+        if (diem > 8.0) {
+            System.out.println("La hs Gioi");
+        } else {
+            if (diem > 6.5) {
+                System.out.println("La hs Kha");
+            } else {
+                if (diem > 4) {
+                    System.out.println("La hs Trung binh");
+                } else {
+                    System.out.println("La hs Yeu");
+                }
+            }
+        }
+    }
 }
