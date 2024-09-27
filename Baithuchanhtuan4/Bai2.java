b71e211ef18fad4b59e7ac1a44183a7bf0a912be
ThoKhang <khangheheqt@gmail.com>
2024-09-27 23:20:28 +0700

    hoan thien bai2

--- Baithuchanhtuan4/Bai2.java
+++ Baithuchanhtuan4/Bai2.java
@@ -3,5 +3,14 @@ package Baithuchanhtuan4;
 import java.util.Scanner;
 
 public class Bai2 {
-    
+    public static void main(String[] args) {
+        System.out.printf("Bạn muốn in cửu chương số mấy: ");
+        int a = new Scanner(System.in).nextInt();
+        if(a>1 && a< 10)
+            for(int i=1; i<=10;i++)
+                System.out.printf("%d * %d = %d\n",a,i,a*i);
+        else
+            System.out.println("không có cửu chương "+a);
+    }
 }
+
