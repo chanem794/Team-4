e0a20947220936de12d3d84e35c8e4b0933542ac
ThoKhang <khangheheqt@gmail.com>
2024-09-27 23:03:11 +0700

    hoan thien bai1 va fix bug bai6

--- Baithuchanhtuan4/Bai1.java
+++ Baithuchanhtuan4/Bai1.java
@@ -1,7 +1,14 @@
 package Baithuchanhtuan4;
 // 1.	Viết chương trình tính tổng các số từ 1 đến n.
 import java.util.Scanner;
-//test
+
 public class Bai1 {
-       
+    public static void main(String[] args) {
+        System.out.printf("Vui lòng nhập số n để tính tổng từ 1 đến n: ");
+        int n = new Scanner(System.in).nextInt();
+        int tong = 0;
+        for(int i=1;i<=n; i++)
+            tong = tong + i;
+        System.out.printf("tổng giá trị từ một đến %d là: %d",n, tong);
+    }
 }
--- Baithuchanhtuan4/Bai6.java
+++ Baithuchanhtuan4/Bai6.java
@@ -1,6 +1,6 @@
 // 6.	Viết chương trình nhập vào một số nguyên và kiểm tra xem số đó có phải là số đối xứng hay không (ví dụ: 121, 1331).
 package Baithuchanhtuan4;
 
-public class bai6 {
+public class Bai6 {
     
 }
