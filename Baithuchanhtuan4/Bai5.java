03cfbed7b9c269f80031f48813675e557dd88d49
ThoKhang <khangheheqt@gmail.com>
2024-09-28 23:37:59 +0700

    hoan thien bai 5

--- Baithuchanhtuan4/Bai5.java
+++ Baithuchanhtuan4/Bai5.java
@@ -3,5 +3,29 @@ package Baithuchanhtuan4;
 import java.util.Scanner;
 
 public class Bai5 {
-    
+    public static void main(String[] args) {
+        System.out.println("Vui lòng nhập hai số a và b để tìm ước chung lớn nhất và bội chung nhỏ nhất: ");
+        System.out.printf("Nhập a: ");
+        int a = new Scanner(System.in).nextInt();
+        System.out.printf("Nhập b: ");
+        int b = new Scanner(System.in).nextInt();
+        //khởi tạo biến ảo để tính toán
+        int c = a, d = b;
+
+        /* kiểm tra ước chung lớn nhất bằng phương pháp trừ dần
+        while(c!=d)
+            if (c>d)
+                c=c-d;
+            else
+                d=d-c;
+                */
+        //kiểm tra ước chung lớn nhất bằng phương pháp ơ-clit
+        while(c%d!=0)
+        {
+            int r= c%d;
+            c=d;
+            d=r;
+        }
+        System.out.printf("Vậy ước chung nhỏ nhất của %d và %d là %d",a,b,d);
+    }
 }
