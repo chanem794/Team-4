5b088e14d3f244604b996cd3725260cae92c713d
ThoKhang <khangheheqt@gmail.com>
2024-09-28 19:46:20 +0700

    hoan thien bai3

--- Baithuchanhtuan4/Bai3.java
+++ Baithuchanhtuan4/Bai3.java
@@ -4,5 +4,22 @@ package Baithuchanhtuan4;
 import java.util.Scanner;
 
 public class Bai3 {
-    
+    public static void main(String[] args) {
+        System.out.printf("Vui lòng nhập gía trị nguyên dương n: ");
+        int n = new Scanner(System.in).nextInt();
+        int kq=1;
+        //Tính tổng theo vòng lặp for
+        for(int i=1; i<=n; i++)
+            kq = kq*i;
+        /*
+        Tính tổng theo vòng lặp while do
+        int i= 1
+        while (i <= n)
+        {
+            kq = kq*i;
+            i+=1;
+        }
+         */
+        System.out.printf("Tổng gia thừa của số %d là: %d",n,kq);
+    }
 }
