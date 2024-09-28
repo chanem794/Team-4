d33c558c417c8cacd14c71ec2f5af1b6a4804d56
ThoKhang <khangheheqt@gmail.com>
2024-09-28 20:33:36 +0700

    hoan thien bai 4

--- Baithuchanhtuan4/Bai4.java
+++ Baithuchanhtuan4/Bai4.java
@@ -3,6 +3,24 @@ package Baithuchanhtuan4;
 import java.util.Scanner;
 
 public class Bai4 {
-    
+    public static void main(String[] args) {
+        System.out.printf("Vui lòng nhập số nguyên: ");
+        int n = new Scanner(System.in).nextInt();
+        if (n<=0)//kiểm tra số nhập vào có phải số nguyên dương hay không
+            System.out.printf("Số %d không phải là một số nguyên dương nên không thể là một số nguyên tố !", n);
+        else
+        {
+            int kq=0;//kởi tạo biến kết quả
+            for(int i=1; i<=n ;i++)
+                if(n%i==0)//kiểm tra n có phải số nguyên tố không
+                    kq=kq+1;
+            if(kq==2)
+                System.out.printf("Vậy số %d là số nguyên tố !", n);
+            else
+                System.out.printf("Vậy số %d không phải là số nguyên tố !", n);
+        }
+
+    }
 }
+
 
