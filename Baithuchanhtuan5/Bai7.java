ee8f8818d63879dd3163df7fe95b6d5bbc41eb7f
Dinh <dinhknd3@gmail.com>
2024-10-03 22:59:04 +0700

    Baithuchanhtuan6

--- Baithuchanhtuan5/Bai7.java
+++ Baithuchanhtuan5/Bai7.java
@@ -1,5 +1,6 @@
 package Baithuchanhtuan5;
-//7.	Tạo một lớp Student với các thuộc tính: mã sinh viên, tên, điểm trung bình. Thực hiện các chức năng: thêm sinh viên, xóa sinh viên, tìm kiếm sinh viên theo mã, sắp xếp sinh viên theo điểm.
+//7.	Tạo một lớp Student với các thuộc tính: mã sinh viên, tên, điểm trung bình. 
+//Viết Thực hiện các chức năng: thêm sinh viên, xóa sinh viên, tìm kiếm sinh viên theo mã, sắp xếp sinh viên theo điểm.
 
 public class Bai1 {
     
---
+++ Baithuchanhtuan6/HinhChuNhat.java
@@ -0,0 +1,12 @@
+package Baithuchanhtuan6;
+
+
+// 1.	Tạo lớp Hình chữ nhật:
+// •	Tạo lớp HinhChuNhat với các thuộc tính: chiều dài, chiều rộng.
+// •	Viết các phương thức tính diện tích, chu vi.
+// •	Tạo đối tượng và in kết quả ra màn hình.
+
+
+public class HinhChuNhat {
+    
+}
