3318d5ce00c6ee92b39646d06cc8682ad845c4b6
Dinh <dinhknd3@gmail.com>
2024-10-03 22:51:54 +0700

    Baithuchanhtuan5

---
+++ Baithuchanhtuan5/Bai1.java
@@ -0,0 +1,6 @@
+package Baithuchanhtuan5;
+// 1.	Viết chương trình để tính tổng các phần tử trong một mảng số nguyên.
+
+public class Bai1 {
+    
+}
---
+++ Baithuchanhtuan5/Bai3.java
@@ -0,0 +1,7 @@
+package Baithuchanhtuan5;
+
+// 3.	Viết chương trình để sắp xếp các phần tử trong một mảng theo thứ tự tăng dần.
+
+public class Bai1 {
+    
+}
