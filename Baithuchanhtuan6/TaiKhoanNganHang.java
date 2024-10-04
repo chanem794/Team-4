de30b31e6e2baed97908342a8e1f8052474e23bf
Dinh <dinhknd3@gmail.com>
2024-10-04 08:27:09 +0700

    Test tuan6

--- Baithuchanhtuan6/TaiKhoanNganHang.java
+++ Baithuchanhtuan6/TaiKhoanNganHang.java
@@ -2,7 +2,7 @@ package Baithuchanhtuan6;
 
 // 3.	Tạo lớp Tài khoản ngân hàng:
 // •	Tạo lớp TaiKhoan với các thuộc tính: số tài khoản, chủ tài khoản, số dư.
-// •	Viết các phương thức gửi tiền, rút tiền, kiểm tra số dư.
+// •	Viết các phương thức gửi tiền, rút tiền, kiểm tra số dư..
 
 
 public class TaiKhoanNganHang {
