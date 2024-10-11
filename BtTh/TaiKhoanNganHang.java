1788979cdc89a4e81f9c039a1fd94b3cea433efe
Dinh <dinhknd3@gmail.com>
2024-10-11 16:06:02 +0700

    Hoạn thiện bai thuc hanh

--- BtTh/TaiKhoanNganHang.java
+++ BtTh/TaiKhoanNganHang.java
@@ -1,43 +1,83 @@
 package BtTh;
-import  java.util.Scanner;
+
+import java.util.Scanner;
+
 public class TaiKhoanNganHang {
     private int stk;
     private String chutk;
     private long sodu;
     private String matKhau;
     public Scanner SC;
+
     public TaiKhoanNganHang(int stk, String chutk, long sodu, String matKhau) {
         this.stk = stk;
         this.chutk = chutk;
         this.sodu = sodu;
+        this.matKhau = matKhau;
+    }
+
+    
+    public TaiKhoanNganHang() {
+    }
+
+    public TaiKhoanNganHang(String chutk, long sodu, int stk) {
+        this.chutk = chutk;
+        this.sodu = sodu;
+        this.stk = stk;
     }
 
     public long getsodu() {
-        return sodu;
+        return this.sodu;
     }
-
-    public long guitien(long tien) {
-        return this.sodu += tien;
-    }
-
-    public long ruttien(long tien) {
-        return this.sodu -= tien;
-    }
-
-    public TaiKhoanNganHang() {
-        SC = new Scanner(System.in);
-        System.out.println("vui lòng nhập số tài khoản: ");
-        this.stk = SC.nextInt();
-        System.out.println("Vui lòng nhập tên người dùng: ");
-        SC.nextLine();
-        this.chutk = SC.nextLine();
-        System.out.println("Vui lòng nhập số tiền cần gửi lần đầu tiên: ");
-        this.sodu = SC.nextLong();
-    }
-
     public void In() {
         System.out.println("Tên người dùng: " + this.chutk);
         System.out.println("Số tài khoản: " + this.stk);
         System.out.println("Số dư: " + this.sodu);
     }
+
+    private boolean kiemTraMatKhau(String matKhauNhap) {
+        return matKhauNhap.equals(this.matKhau);
+    }
+
+    public void guiTien(long soTien) {
+        System.out.println("Vui lòng nhập mật khẩu:");
+        String matKhauNhap = SC.next();
+        if (kiemTraMatKhau(matKhauNhap)) {
+            if (soTien > 0) {
+                this.sodu += soTien;
+                System.out.println("Gửi tiền thành công. Số dư mới: " + this.sodu);
+            } else {
+                System.out.println("Số tiền gửi phải lớn hơn 0.");
+            }
+        } else {
+            System.out.println("Mật khẩu không đúng.");
+        }
+    }
+
+    public void rutTien(long soTien) {
+        System.out.println("Vui lòng nhập mật khẩu:");
+        String matKhauNhap = SC.next();
+        if (kiemTraMatKhau(matKhauNhap)) {
+            if (soTien > 0 && soTien <= this.sodu) {
+                this.sodu -= soTien;
+                System.out.println("Rút tiền thành công. Số dư mới: " + this.sodu);
+            } else {
+                System.out.println("Số tiền rút phải lớn hơn 0 và không vượt quá số dư.");
+            }
+        } else {
+            System.out.println("Mật khẩu không đúng.");
+        }
+    }
+
+    public void kiemTraSoDu() {
+        System.out.println("Vui lòng nhập mật khẩu:");
+        String matKhauNhap = SC.next();
+        if (kiemTraMatKhau(matKhauNhap)) {
+            System.out.println("Số dư hiện tại: " + this.sodu);
+        } else {
+            System.out.println("Mật khẩu không đúng.");
+        }
+    }
+
+    
 }
---
+++ BtTh/test.java
@@ -0,0 +1,61 @@
+package BtTh;
+import java.util.Scanner;
+public class test {
+    public static void main(String[] args) {
+        Scanner SC = new Scanner(System.in);
+        TaiKhoanNganHang a = new TaiKhoanNganHang();
+        int b;
+        long kq;
+        System.out.println("=====================================");
+        System.out.println("QUẢN LÝ TÀI KHOẢN NGÂN HÀNG");
+        System.out.println("=====================================");
+        System.out.print("Vui lòng nhập số tài khoản: ");
+        String soTaiKhoan = SC.next();
+        System.out.print("Vui lòng nhập tên người dùng: ");
+        String tenNguoiDung = SC.next();
+        System.out.print("Vui lòng nhập số tiền cần gửi lần đầu tiên: ");
+        long soTienCanGui = SC.nextLong();
+        System.out.print("Vui lòng nhập mật khẩu: ");
+        String matKhau = SC.next();
+        System.out.println("=====================================");
+        do
+        {
+            System.out.println("Vui lòng chọn một trong các tùy chọn sau:");
+            System.out.println("1. Kiểm tra toàn bộ tài khoản");
+            System.out.println("2. Kiểm tra số dư");
+            System.out.println("3. Gửi tiền vào tài khoản");
+            System.out.println("4. Rút tiền");
+            System.out.println("5. Kết thúc");
+            b = SC.nextInt();
+            switch (b)
+            {
+                case 1:
+                    System.out.println("Thông tin tài khoản:");
+                    System.out.println("Tên người dùng: " + tenNguoiDung);
+                    System.out.println("Số tài khoản: " + soTaiKhoan);
+                    System.out.println("Số dư: " + a.getsodu());
+                    break;
+                case 2:
+                    System.out.println("Số dư hiện tại: " + a.getsodu());
+                    break;
+                case 3:
+                    System.out.print("Vui lòng nhập số tiền muốn gửi: ");
+                    kq = SC.nextLong();
+                    a.guiTien(kq);
+                    break;
+                case 4:
+                    System.out.print("Vui lòng nhập số tiền muốn rút: ");
+                    kq = SC.nextLong();
+                    a.rutTien(kq);
+                    break;
+                case 5:
+                    System.out.println("Đang kết thúc.....");
+                    break;
+                default:
+                    System.out.println("Nhập không đúng! ");
+                    break;
+            }
+        }
+        while (b != 5);
+    }
+}
